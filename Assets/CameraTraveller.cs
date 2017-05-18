81853b57263fe68b198c8a5891aae66477b6bf4d
batoust <thebatoust@gmail.com>
2017-05-18 23:54:18 +0200

    Dayum noice meine menu

---
+++ Assets/CameraTraveller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class CameraTraveller : MonoBehaviour {
+
+    [SerializeField]
+    private float travelDuration;
+
+    [SerializeField]
+    private Transform[] travelPositions;
+
+    private Sequence currentSequence = null;
+
+    public void GoToTravelPosition(int index)
+    {
+        if (currentSequence != null)
+        {
+            currentSequence.Kill();
+            currentSequence = null;
+        }
+
+        currentSequence = DOTween.Sequence();
+
+        currentSequence
+            .Append(transform.DOMove(travelPositions[index].position, travelDuration))
+            .Insert(0f, transform.DORotate(travelPositions[index].eulerAngles, travelDuration));
+
+        currentSequence.Play();
+    }
+
+}
---
+++ Assets/Sources/UI/EventSystemAlwaysFocusing.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class EventSystemAlwaysFocusing : MonoBehaviour {
+
+    EventSystem es;
+    private GameObject mainFocus;
+
+    private void Awake()
+    {
+        es = GetComponent<EventSystem>();
+    }
+
+    private void Start()
+    {
+        mainFocus = es.firstSelectedGameObject;
+
+        es.SetSelectedGameObject(null);
+        es.SetSelectedGameObject(mainFocus);
+        mainFocus.GetComponent<Selectable>().Select();
+        //mainFocus.GetComponent<Image>().sprite = mainFocus.GetComponent<Selectable>().spriteState.highlightedSprite;
+    }
+
+    void Update () {
+        if (es.currentSelectedGameObject == null && !es.alreadySelecting)
+        {
+            es.SetSelectedGameObject(null);
+            es.SetSelectedGameObject(mainFocus);
+            mainFocus.GetComponent<Selectable>().Select();
+            //mainFocus.GetComponent<Image>().sprite = mainFocus.GetComponent<Selectable>().spriteState.highlightedSprite;
+        }
+            
+	}
+
+    public void SetMainFocus(GameObject focus)
+    {
+        es.SetSelectedGameObject(null);
+        mainFocus = focus;
+        es.SetSelectedGameObject(mainFocus);
+        mainFocus.GetComponent<Selectable>().Select();
+        //mainFocus.GetComponent<Image>().sprite = mainFocus.GetComponent<Selectable>().spriteState.highlightedSprite;
+    }
+
+    public void RemoveFocus()
+    {
+        es.SetSelectedGameObject(null);
+    }
+}
